058d1a5be32cb3a3d47a3fd5ad21e0afbdda44b3
Meghan Adkins <meghandadkins@gmail.com>
2019-07-25 11:33:37 -0400

    more grid layout edits

--- src/components/SummaryPage.js
+++ src/components/SummaryPage.js
@@ -1,24 +1,39 @@
 import React from 'react'
 import styled from 'styled-components'
 
-
-
 const StyledSummaryPage = styled.div`
-.GridWrapper {
+  .GridWrapper {
+    max-width: 80%;
+    margin: 0 auto;
     display: grid;
+    grid-template-columns: repeat(3, 1fr);
     grid-gap: 10px;
-    grid-template-columns: repeat(3, 80px);
-    grid-template-rows: repeat(3, 200px);
-    align-content: space-around;
-    justify-content: space-between;
+    grid-auto-rows: minmax(10px, auto);
+    grid-template-rows: 55px calc(100vh - 55px);
+  }
+  .Header {
+    grid-column: 1 / 4;
+    grid-row: 1;
+  }
+  .Content {
+    grid-column: 1;
+    grid-row: 2 / 5;
+    overflow: auto;
   }
   .LeftScrollCell {
-    grid-column: 1 / 3;
-    grid-row: 1 / 4;
+    height: 100px;
   }
   .TopRightStaticCell {
-    margin-top: 12px;
-    grid-column: 3 / 5;
+    grid-column: 2/4;
+    grid-row: 2/4;
+  }
+  .DoughnutLeft {
+    grid-column: 2;
+    grid-row: 4;
+  }
+  .DoughnutRight {
+    grid-column: 3;
+    grid-row: 4;
   }
 `;
 
@@ -26,23 +41,29 @@ export default function SummaryPage() {
     return (
         <StyledSummaryPage>
             <div className='GridWrapper'>
-                <div className='LeftScrollCell'>
-                    <span>Hello from budget list</span>
-                    <ul>
-                        <li>There</li>
-                        <li>Will</li>
-                        <li>Be</li>
-                        <li>A</li>
-                        <li>List</li>
-                        <li>Here</li>
-                    </ul>
+                <div className='Header'>Hello from header</div>
+                <div className='Content'>
+                    <div className='LeftScrollCell'>
+                        <span>Hello from budget list</span>
+                        <ul>
+                            <li>There</li>
+                            <li>Will</li>
+                            <li>Be</li>
+                            <li>A</li>
+                            <li>List</li>
+                            <li>Here</li>
+                        </ul>
+                    </div>
                 </div>
-                <div className='TopRightStaticCell'>
-                    <span>Hello from budget overview</span>
-                </div>
-                <div className='BottomRightStaticCell'>
-                    <span>Hello from doughnut charts</span>
-                </div>
+                    <div className='TopRightStaticCell'>
+                        <span>Hello from budget overview</span>
+                    </div>
+                    <div className="DoughnutLeft">
+                        <span>Hello from doughnut charts</span>
+                    </div>
+                    <div className="DoughnutRight">
+                        <span>Hello from doughnut charts</span>
+                    </div>
             </div>
         </StyledSummaryPage>
     )
